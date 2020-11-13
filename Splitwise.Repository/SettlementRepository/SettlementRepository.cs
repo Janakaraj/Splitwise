94b5e910a7e7e30dcfad787eb4f6882eebc4435e
Janakaraj <janak@promactinfo.com>
2020-11-13 12:09:14 +0530

    Fixes bad return object

--- Splitwise.Repository/SettlementRepository/SettlementRepository.cs
+++ Splitwise.Repository/SettlementRepository/SettlementRepository.cs
@@ -82,7 +82,13 @@ namespace Splitwise.Repository.SettlementRepository
 
         public async Task<IEnumerable<SettlementAC>> GetSettlementsByExpenseId(int expenseId)
         {
-            return this._mapper.Map<IEnumerable<SettlementAC>>(await this._context.Settlements.Where(e => e.SettlementExpenseId == expenseId).ToListAsync());
+            return this._mapper.Map<IEnumerable<SettlementAC>>(await this._context.Settlements
+                .Where(e => e.SettlementExpenseId == expenseId)
+                .Include(e => e.SettlementExpense)
+                .Include(e => e.Group)
+                .Include(e => e.UserPaying)
+                .Include(e => e.UserRecieving)
+                .ToListAsync());
         }
 
         public bool SettlementExists(int settlementId)
